484b09f9035a99fb6efec7cf278b1839a0708f29
Joshua Gretton <jb.gretton@googlemail.com>
2022-04-15 16:09:48 +0100

    Updated tailwind with data from the style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Nunito: ["Nunito Sans", "sans-serif"],
+      },
+      colors: {
+        DarkBlue: "hsl(209, 23%, 22%)",
+        VDarkBlueDBG: "hsl(207, 26%, 17%)",
+        VDarkBlueLText: "hsl(200, 15%, 8%)",
+        DarkGray: "hsl(0, 0%, 52%)",
+        VeryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
 };
